e0b8b8a51f240ccf9c98be6f00719e07ee2b86c0
Alice Lui <54395483+al01010@users.noreply.github.com>
2025-09-17 16:30:08 +0100

    EPBR-9139: Add to addresses gateway method to search by postcode
    Add additional tests to search_by_building_number_and_postcode

--- lib/gateway/addresses_gateway.rb
+++ lib/gateway/addresses_gateway.rb
@@ -2,11 +2,12 @@ module Gateway
   class AddressesGateway
     def search_by_building_number_and_postcode(building_numbers:, postcode:)
       insert_sql = <<-SQL
-        SELECT fulladdress,#{' '}
+        SELECT fulladdress,
                postcode,
-               uprn
+               uprn,
+               parentuprn
         FROM addresses
-        WHERE postcode = $2#{' '}
+        WHERE postcode = $2
         AND to_tsvector('simple', fulladdress) @@ to_tsquery('simple', REPLACE($1, ' ', ' & '));
       SQL
 
@@ -16,6 +17,27 @@ module Gateway
           building_numbers,
           ActiveRecord::Type::String.new,
         ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "postcode",
+          postcode,
+          ActiveRecord::Type::String.new,
+        ),
+      ]
+
+      ActiveRecord::Base.connection.exec_query(insert_sql, "SQL", bindings)
+    end
+
+    def search_by_postcode(postcode:)
+      insert_sql = <<-SQL
+        SELECT fulladdress,
+               postcode,
+               uprn,
+               parentuprn
+        FROM addresses
+        WHERE postcode = $1
+      SQL
+
+      bindings = [
         ActiveRecord::Relation::QueryAttribute.new(
           "postcode",
           postcode,
--- spec/unit/gateway/addresses_gateway_spec.rb
+++ spec/unit/gateway/addresses_gateway_spec.rb
@@ -5,16 +5,71 @@ describe Gateway::AddressesGateway do
 
   include_context "when accessing addresses table"
 
-  describe "#search_by_building_number_and_postcode" do
-    before do
-      import_sample_data
-    end
+  before(:all) do
+    import_sample_data
+  end
 
-    it "returns addresses that contain the building number and postcode" do
+  describe "#search_by_building_number_and_postcode" do
+    it "returns 1 match that contain the building number and postcode" do
       building_numbers = "29"
       postcode = "EX1 2FW"
       result = gateway.search_by_building_number_and_postcode(building_numbers:, postcode:)
       expect(result.rows.length).to eq 1
+    end
+
+    it "returns the correct values" do
+      building_numbers = "29"
+      postcode = "EX1 2FW"
+      result = gateway.search_by_building_number_and_postcode(building_numbers:, postcode:)
+      expect(result.columns.sort).to eq %w[fulladdress parentuprn postcode uprn]
+    end
+
+    it "returns more than one match that contain the building number and postcode" do
+      building_numbers = "4"
+      postcode = "EX4 4GX"
+      result = gateway.search_by_building_number_and_postcode(building_numbers:, postcode:)
+      expect(result.rows.length).to eq 3
+    end
+
+    it "returns 0 when there is no matches containing the building number and postcode" do
+      building_numbers = "1111"
+      postcode = "B61 4GX"
+      result = gateway.search_by_building_number_and_postcode(building_numbers:, postcode:)
+      expect(result.rows.length).to eq 0
+    end
+
+    it "returns matches when there are multiple building numbers and postcode" do
+      building_numbers = "14 48"
+      postcode = "EX4 4EP"
+      result = gateway.search_by_building_number_and_postcode(building_numbers:, postcode:)
+      expect(result.rows.length).to eq 1
+    end
+
+    it "returns matches when the building number includes a letter" do
+      building_numbers = "6A"
+      postcode = "EX4 1RB"
+      result = gateway.search_by_building_number_and_postcode(building_numbers:, postcode:)
+      expect(result.rows.length).to eq 1
+    end
+  end
+
+  describe "#search_by_postcode" do
+    it "returns all the matches that contain the postcode" do
+      postcode = "EX4 4GX"
+      result = gateway.search_by_postcode(postcode:)
+      expect(result.rows.length).to eq 6
+    end
+
+    it "returns the correct values" do
+      postcode = "EX1 2FW"
+      result = gateway.search_by_postcode(postcode:)
+      expect(result.columns.sort).to eq %w[fulladdress parentuprn postcode uprn]
+    end
+
+    it "returns 0 when there are no matches that contain the postcode" do
+      postcode = "B61 4GX"
+      result = gateway.search_by_postcode(postcode:)
+      expect(result.rows.length).to eq 0
     end
   end
 end
