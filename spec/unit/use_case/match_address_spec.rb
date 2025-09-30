527095990196197236d2b745668e14b68077dd64
Alice Lui <54395483+al01010@users.noreply.github.com>
2025-09-30 14:41:22 +0100

    EPBR-9139: Add additional test when there are no exact matches
    Add more coverage for step 1A and beyond

--- spec/unit/use_case/match_address_spec.rb
+++ spec/unit/use_case/match_address_spec.rb
@@ -40,7 +40,7 @@ describe UseCase::MatchAddress do
         },
         {
           "uprn" => "1000000003",
-          "parent_uprn" => nil,
+          "parent_uprn" => "",
           "full_address" => "FLAT 2, BUILDING 2, 23 COLET PARK, HUMMING CITY, H14 9YA",
           "postcode" => postcode,
         },
@@ -51,7 +51,7 @@ describe UseCase::MatchAddress do
       [
         {
           "uprn" => "2000000001",
-          "parent_uprn" => nil,
+          "parent_uprn" => "",
           "full_address" => "BUILDING 2, 23 COLET PARK, HUMMING CITY, H14 9YA",
           "postcode" => postcode,
         },
@@ -77,28 +77,30 @@ describe UseCase::MatchAddress do
     before do
       allow(find_matches_use_case).to receive(:execute).and_return(find_matches_result)
       allow(find_parents_use_case).to receive(:execute).and_return(find_parents_result)
-      use_case.execute(address:, postcode:)
     end
 
     context "when calling the FindMatches use case" do
       it "extracts the building numbers before calling the use case" do
+        use_case.execute(address:, postcode:)
         expect(find_matches_use_case).to have_received(:execute).with(building_numbers: "1 2 2 23", postcode: anything)
       end
 
       it "passes the postcode to the use case" do
+        use_case.execute(address:, postcode:)
         expect(find_matches_use_case).to have_received(:execute).with(building_numbers: anything, postcode:)
       end
     end
 
     context "when calling the FindParents use case" do
       it "extracts the parent uprns from the FindMatches result" do
+        use_case.execute(address:, postcode:)
         expect(find_parents_use_case).to have_received(:execute).with(uprns: %w[2000000001 2000000001])
       end
     end
 
-    context "when doing a successful search" do
+    context "when we have an exact match" do
       it "returns the expected result" do
-        expect(find_matches_result).to eq(expected_result)
+        expect(use_case.execute(address:, postcode:)).to eq(expected_result)
       end
     end
 
@@ -154,7 +156,101 @@ describe UseCase::MatchAddress do
       end
 
       it "returns the expected result not setting building_number_exact" do
-        expect(find_matches_result).to eq(expected_result)
+        expect(use_case.execute(address:, postcode:)).to eq(expected_result)
+      end
+    end
+
+    context "when set of potential matches do not have an exact match" do
+      let(:address) do
+        "FLAT 2, THIRD BUILDING, COLET PARK, HUMMING CITY"
+      end
+
+      let(:postcode) do
+        "H14 9YA"
+      end
+
+      let(:find_matches_result) do
+        [
+          {
+            "uprn" => "1000000011",
+            "parent_uprn" => "2000000001",
+            "full_address" => "FLAT 2-2, BUILDING 1, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+          },
+          {
+            "uprn" => "1000000002",
+            "parent_uprn" => "",
+            "full_address" => "FLAT 1, BUILDING 2, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+          },
+          {
+            "uprn" => "1000000003",
+            "parent_uprn" => "",
+            "full_address" => "FLAT 2, BUILDING 2, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+          },
+        ]
+      end
+
+      let(:find_parents_result) do
+        [
+          {
+            "uprn" => "2000000001",
+            "parent_uprn" => "",
+            "full_address" => "2 BUILDING THIRD, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+          },
+        ]
+      end
+
+      let(:expected_result) do
+        [
+          {
+            "uprn" => "1000000011",
+            "parent_uprn" => "2000000001",
+            "full_address" => "FLAT 2-2, BUILDING 1, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+            "clean_address" => "FLAT 2 2 BUILDING 1 23 COLET PARK HUMMING CITY H14 9YA",
+            "building_tokens" => 4,
+            "count_building_num_intersect" => 1,
+            "count_tokens_intersect" => 9,
+          },
+          {
+            "uprn" => "1000000002",
+            "parent_uprn" => "",
+            "full_address" => "FLAT 1, BUILDING 2, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+            "clean_address" => "FLAT 1 BUILDING 2 23 COLET PARK HUMMING CITY H14 9YA",
+            "building_tokens" => 3,
+            "count_building_num_intersect" => 1,
+            "count_tokens_intersect" => 9,
+          },
+          {
+            "uprn" => "1000000003",
+            "parent_uprn" => "",
+            "full_address" => "FLAT 2, BUILDING 2, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+            "clean_address" => "FLAT 2 BUILDING 2 23 COLET PARK HUMMING CITY H14 9YA",
+            "building_tokens" => 3,
+            "count_building_num_intersect" => 1,
+            "count_tokens_intersect" => 9,
+          },
+          {
+            "uprn" => "2000000001",
+            "parent_uprn" => "",
+            "full_address" => "2 BUILDING THIRD, 23 COLET PARK, HUMMING CITY, H14 9YA",
+            "postcode" => postcode,
+            "clean_address" => "2 BUILDING THIRD 23 COLET PARK HUMMING CITY H14 9YA",
+            "building_tokens" => 2,
+            "count_building_num_intersect" => 1,
+            "count_tokens_intersect" => 9,
+            "is_parent" => 1,
+          },
+        ]
+      end
+
+      it "returns the expected result" do
+        expect(use_case.execute(address:, postcode:)).to eq(expected_result)
       end
     end
   end
